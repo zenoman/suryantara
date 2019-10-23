b2db52aab7b48c8cf300c48b7e005a970ea799f0
devsatrio <satriosuklun@gmail.com>
2019-10-23 13:41:56 +0700

    Update Dashboardcontroller.php

--- app/Http/Controllers/Dashboardcontroller.php
+++ app/Http/Controllers/Dashboardcontroller.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class Dashboardcontroller extends Controller {
     public function __construct()
